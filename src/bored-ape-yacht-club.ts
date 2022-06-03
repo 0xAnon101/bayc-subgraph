faf852a69293bcc37f64aaa40f7f247e1b454248
Raju <rajugautam45@gmail.com>
2022-06-03 17:43:29 +0530

    bayc graphql controller

--- src/bored-ape-yacht-club.ts
+++ src/bored-ape-yacht-club.ts
@@ -1,6 +1,9 @@
-import { log } from "@graphprotocol/graph-ts";
+import { log, ipfs } from "@graphprotocol/graph-ts";
 import { Transfer as TransferEvent } from "../generated/BoredApeYachtClub/BoredApeYachtClub";
-import { BoredApeToken, BoredApeUser } from "../generated/schema";
+import {
+  BoredApeToken,
+  // BoredApeUser
+} from "../generated/schema";
 
 export function handleTransfer(event: TransferEvent): void {
   /**
@@ -9,6 +12,8 @@ export function handleTransfer(event: TransferEvent): void {
   const baseHash = "QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq";
   const ipfsURI = `ipfs.io/ipfs/${baseHash}/`;
 
+  log.info("IPFS URL: {}", [ipfsURI]);
+
   /**
    * load the BAYC if it exists other wise generate new BAYC entity instance
    * can also use event.params.id.toHex()
@@ -16,7 +21,14 @@ export function handleTransfer(event: TransferEvent): void {
   let entityToken = BoredApeToken.load(event.transaction.from.toHex());
   if (!entityToken) {
     entityToken = new BoredApeToken(event.transaction.from.toHex());
+    entityToken.tokenID = event.params.tokenId;
+
+    // entityToken.owner = event.params.to.toHexString();
+    entityToken.contentURI = `${ipfsURI}/${entityToken.tokenID}`;
+    log.info("CONTENT URI {} ", [entityToken.contentURI]);
   }
+
+  entityToken.save();
 
   // entity.owner = event.params.to.toHex();
 
@@ -24,10 +36,10 @@ export function handleTransfer(event: TransferEvent): void {
    * load the BAYC if it exists other wise generate new BAYC entity instance
    * can also use event.params.id.toHex()
    **/
-  let entityUser = BoredApeUser.load(event.transaction.from.toHex());
-  if (!entityUser) {
-    entityUser = new BoredApeUser(event.transaction.from.toHex());
-  }
+  // let entityUser = BoredApeUser.load(event.transaction.from.toHex());
+  // if (!entityUser) {
+  //   entityUser = new BoredApeUser(event.transaction.from.toHex());
+  // }
 
-  entityUser.save();
+  // entityUser.save();
 }
